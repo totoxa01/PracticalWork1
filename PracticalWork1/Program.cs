252903fa1c8308f65915f40bf6525e1fcb609fba
glowwormsv <adm_kot@mail.ru>
2021-03-31 19:37:49 +0300

    Add project files.

---
+++ PracticalWork1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PracticalWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня "+DateTime.Now);
+            Console.ReadKey();
+        }
+    }
+}
